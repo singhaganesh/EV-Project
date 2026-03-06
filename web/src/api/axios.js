64a03dd0755b8711b9c3f0a35e80002903ffdf4c
Ganesh Singh <ganeshsingha741@gmail.com>
2026-03-06 10:01:52 +0530

    switching localhost backend to AWS

--- web/src/api/axios.js
+++ web/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', // Adjust base URL as needed based on production/env
+    baseURL: 'http://ev-charging-backend-env.eba-bppan2jb.eu-north-1.elasticbeanstalk.com/api', // Adjust base URL as needed based on production/env
     headers: {
         'Content-Type': 'application/json',
     },
